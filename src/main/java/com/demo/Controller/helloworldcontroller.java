29f6373125f70f8271d653b34301c87fa3dce6f5
narwateanusaya@gmail.com <vijaypole121@gmail.comclear>
2025-04-04 18:36:44 +0530

    demo-app-v1

---
+++ src/main/java/com/demo/Controller/helloworldcontroller.java
@@ -0,0 +1,14 @@
+package com.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloworldcontroller {
+	
+	@GetMapping("/Messagr")
+	public String getMessage() {
+		return "hello-world";
+	}
+
+}
